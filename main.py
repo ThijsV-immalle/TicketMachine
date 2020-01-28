86c8ca7ff949276e5db855196077b051e184ef02
ThijsV-immalle <thijs.verbelen@immalle.be>
2020-01-28 11:24:37 +0100

    afgewerkteOpdracht

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ class TicketMachine:
     def __init__(self, cost):
         self.cost = cost
         self.balance = 0
-        pass
 
     def get_balance(self):
         return self.balance
@@ -13,28 +12,49 @@ class TicketMachine:
 
     def insert_money(self, amount):
         self.balance += amount
-        return self.balance
 
     def print_ticket(self):
         if self.balance >= self.cost:
-            print("Hier is je ticket")
+            print("""
+                        <|#################|>
+                       < |Ticket 4562      | >
+                      <  |Zaal 3           |  >
+                      <  |Rij 12           |  >
+                       < |Stoel 6          | >
+                        <|#################|>
+            """)
             self.balance -=self.cost
         else:
             print("Je hebt niet voldoende geld gegeven!")
         
-        return self.balance
         
-
     def refund_balance(self):
+        print(f"Je hebt {self.balance} teruggekregen")
         self.balance = 0
-        return self.balance
+        
 
 
 if __name__ == "__main__":
     t1 = TicketMachine(50)
     t2 = TicketMachine(30)  
 
-    t1.insert_money(80)
-    print(t1.balance)
-    print(t1.print_ticket())
-    
+    print("""
+            |***********************|
+            |Een ticket kost 50 cent|
+            |***********************|
+            """)
+
+    t1.insert_money(70) #Je steekt 70 centjes in de de machine
+    print("Je hebt geld in de machine gestoken :D")
+    print(f"Ticket balance: {t1.get_balance()}") #Je vraagt de balance op --> 70
+    t1.print_ticket() #Je print het ticket --> in dit geval heb je genoeg geïnsert om een ticket te kopen
+    print()
+    print(f"Ticket balance: {t1.get_balance()}") #Je vraagt opnieuw de balance op --> in dit geval 20 omdat er 50 is afgetrokken voor het ticket
+    print()
+    t1.print_ticket() #Je print het ticket --> 20 is niet genoeg om een ticket te kopen
+    print()
+    print(f"Ticket balance: {t1.get_balance()}") #Je vraagt opnieuw de balance op --> 20
+    print()
+    t1.refund_balance() #Dit zorgt ervoor dat je de 20 centjes terugkrijgt
+    print()
+    print(f"Ticket balance: {t1.get_balance()}") #Nu zie je dat je het geld hebt teruggekregen omdat de balance nu 0 is
